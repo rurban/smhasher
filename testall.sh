6bf9739e99b0ebecea36174b01e951a710410170
Reini Urban <rurban@cpanel.net>
2015-06-17 20:31:07 +0200

    testall.sh: use cmake in build dir

--- testall.sh
+++ testall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-make -s
+make -C build
 test -f log.hashes && mv log.hashes log.hashes.bak
-(for g in `./SMHasher --list | cut -s -f1`; do ./SMHasher $g; done) > log.hashes
+(for g in `build/SMHasher --list | cut -s -f1`; do build/SMHasher $g; done) | tee log.hashes
