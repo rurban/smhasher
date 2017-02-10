93e5358e3281df20d8d66dc71f9608d72c1eac71
Leo Yuriev <leo@yuriev.ru>
2017-02-10 17:22:30 +0300

    t1ha: update for mainstream (fixes for MSVC and GCC 4.4)

--- t1ha.c
+++ t1ha.c
@@ -39,25 +39,34 @@
  */
 
 #include "t1ha.h"
-#include <string.h>
+#include <string.h> /* for memcpy() */
 
 #if !defined(__BYTE_ORDER__) || !defined(__ORDER_LITTLE_ENDIAN__) ||           \
     !defined(__ORDER_BIG_ENDIAN__)
+#ifndef _MSC_VER
+#include <sys/param.h> /* for endianness */
+#endif
+#if defined(__BYTE_ORDER) && defined(__LITTLE_ENDIAN) && defined(__BIG_ENDIAN)
+#define __ORDER_LITTLE_ENDIAN__ __LITTLE_ENDIAN
+#define __ORDER_BIG_ENDIAN__ __BIG_ENDIAN
+#define __BYTE_ORDER__ __BYTE_ORDER
+#else
 #define __ORDER_LITTLE_ENDIAN__ 1234
 #define __ORDER_BIG_ENDIAN__ 4321
-#if defined(__LITTLE_ENDIAN__) || defined(__ARMEL__) ||                        \
-    defined(__THUMBEL__) || defined(__AARCH64EL__) || defined(__MIPSEL__) ||   \
-    defined(_MIPSEL) || defined(__MIPSEL) || defined(__i386) ||                \
-    defined(__x86_64__) || defined(_M_IX86) || defined(_M_X64) ||              \
-    defined(i386) || defined(_X86_) || defined(__i386__) ||                    \
-    defined(_X86_64_) || defined(_M_ARM)
+#if defined(__LITTLE_ENDIAN__) || defined(_LITTLE_ENDIAN) ||                   \
+    defined(__ARMEL__) || defined(__THUMBEL__) || defined(__AARCH64EL__) ||    \
+    defined(__MIPSEL__) || defined(_MIPSEL) || defined(__MIPSEL) ||            \
+    defined(__i386) || defined(__x86_64__) || defined(_M_IX86) ||              \
+    defined(_M_X64) || defined(i386) || defined(_X86_) || defined(__i386__) || \
+    defined(_X86_64_) || defined(_M_ARM) || defined(__e2k__)
 #define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
-#elif defined(__BIG_ENDIAN__) || defined(__ARMEB__) || defined(__THUMBEB__) || \
-    defined(__AARCH64EB__) || defined(__MIPSEB__) || defined(_MIPSEB) ||       \
-    defined(__MIPSEB)
+#elif defined(__BIG_ENDIAN__) || defined(_BIG_ENDIAN) || defined(__ARMEB__) || \
+    defined(__THUMBEB__) || defined(__AARCH64EB__) || defined(__MIPSEB__) ||   \
+    defined(_MIPSEB) || defined(__MIPSEB)
 #define __BYTE_ORDER__ __ORDER_BIG_ENDIAN__
 #else
 #error __BYTE_ORDER__ should be defined.
+#endif
 #endif
 #endif
 
@@ -88,7 +97,9 @@
 #endif
 #define likely(cond) __builtin_expect(!!(cond), 1)
 #define unlikely(cond) __builtin_expect(!!(cond), 0)
+#if __GNUC_PREREQ(4, 5) || defined(__clang__)
 #define unreachable() __builtin_unreachable()
+#endif
 #define bswap64(v) __builtin_bswap64(v)
 #define bswap32(v) __builtin_bswap32(v)
 #if __GNUC_PREREQ(4, 8) || __has_builtin(__builtin_bswap16)
@@ -100,8 +111,20 @@
 
 #elif defined(_MSC_VER)
 
-#pragma warning(disable:4710) /* C4710: C4710: 'mux64': function not inlined */
-#pragma warning(disable:4711) /* C4711: function 'x86_cpu_features' selected for automatic inline expansion */
+#if _MSC_FULL_VER < 190024215
+#if _MSC_FULL_VER < 180040629 && defined(_M_IX86)
+#error Please use Visual Studio 2015 (MSC 19.0) or newer for 32-bit target.
+#else
+#pragma message(                                                               \
+    "It is recommended to use Visual Studio 2015 (MSC 19.0) or newer.")
+#endif
+#endif
+
+#pragma warning(                                                               \
+    disable : 4710) /* C4710: C4710: 'mux64': function not inlined */
+#pragma warning(                                                               \
+    disable : 4711) /* C4711: function 'x86_cpu_features' selected for         \
+                       automatic inline expansion */
 
 #include <intrin.h>
 #include <stdlib.h>
@@ -113,6 +136,7 @@
 #define bswap16(v) _byteswap_ushort(v)
 #define rot64(v, s) _rotr64(v, s)
 #define rot32(v, s) _rotr(v, s)
+#define __inline __forceinline
 
 #if defined(_M_ARM64) || defined(_M_X64)
 #pragma intrinsic(_umul128)
