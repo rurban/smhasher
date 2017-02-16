4d82efc257830289c4dd8b606912236a89b6aa74
Yves Orton <demerphq@gmail.com>
2017-02-16 22:05:03 +0100

    change table output

--- doc_report.pl
+++ doc_report.pl
@@ -48,35 +48,41 @@ foreach my $file (@files) {
 
 use Data::Dumper;
 my @rows;
-my $l1;
-my $l2;
+my $last_speed;
 foreach my $data (values %funcs) {
-    $l1 //= $data->{small_speed}[1]{len};
-    $l2 //= $data->{small_speed}[-1]{len};
+    $last_speed //= 0+@{$data->{small_speed}};
     push @rows, [
-        $data->{ok},
         $data->{name},
+        $data->{ok},
         $data->{took},
         $data->{align}[0]{bytescycle},
         $data->{align}[0]{mibsec},
+        $data->{small_speed}[0]{cycles_hash},
         $data->{small_speed}[1]{cycles_hash},
         $data->{small_speed}[-1]{cycles_hash},
     ];
 }
-push @sort, 1;
+push @sort, '+1', 2 if !@sort;
+push @sort, '+1', '0';
 @sort= map { split/\s*,\s*/, $_ } @sort;
 my @sort_dir= map { s/^\+// ? 1 : 0 } @sort;
 
 no warnings 'numeric';
+printf "%-20s %-4s %5s %5s %8s %7s %7s %7s\n",
+    "Hash Name", "Ok?", "Took", "b/c", "MiB/sec", "L0 b/h", "L1 b/h", "L$last_speed b/h";
+printf "%-20s %-4s %5s %5s %8s %7s %7s %7s\n",
+    "-" x 20, "-" x 4, "-" x 5, "-" x 5, "-" x 8, "-" x 7, "-" x 7, "-" x 7;
 foreach my $row (sort {
     my $res= 0;
     map $res ||= $sort_dir[$_]
-                 ? ($b->[$sort[$_]] <=> $a->[$sort[$_]] || $b->[$sort[$_]] cmp $a->[$sort[$_]])
-                 : ($a->[$sort[$_]] <=> $b->[$sort[$_]] || $a->[$sort[$_]] cmp $b->[$sort[$_]])
+                 ? ($b->[$sort[$_]] <=> $a->[$sort[$_]] ||
+                    lc($b->[$sort[$_]]) cmp lc($a->[$sort[$_]]))
+                 : ($a->[$sort[$_]] <=> $b->[$sort[$_]] ||
+                    lc($a->[$sort[$_]]) cmp lc($b->[$sort[$_]]))
              , 0 .. $#sort;
     $res
 } @rows)
 {
-    printf "%4s %-20s took:%5ds Al0 %s b/c %9.2f Mbs L$l1:%s L$l2:%s\n",
-        $row->[0] ? "PASS" : "FAIL", @$row[1..$#$row];
+    printf "%-20s %4s %5d %5.3f %8.2f %7.3f %7.3f %7.3f\n",
+        $row->[0], $row->[1] ? "PASS" : "FAIL", @$row[2..$#$row];
 }
