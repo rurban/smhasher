ae0016619519c047a3e34b73dbc66d0a7ca903bc
Yves Orton <demerphq@gmail.com>
2017-02-21 22:20:08 +0100

    add a way to calculate the birthday number for our hashes
    to be used later

--- Stats.h
+++ Stats.h
@@ -8,14 +8,19 @@
 #include <algorithm>   // for std::sort
 #include <string.h>    // for memset
 #include <stdio.h>     // for printf
-
+#include <gsl/gsl_sys.h>
 double calcScore ( const int * bins, const int bincount, const int ballcount );
 
 void plot ( double n );
 
 inline double ExpectedCollisions ( double balls, double bins )
 {
-  return balls - bins + bins * pow(1 - 1/bins,balls);
+  return balls - bins + bins * pow(1.0 - 1.0/bins, balls);
+}
+
+inline double BirthdayCount(double probability, int bits )
+{
+    return sqrt( 2.0 * pow(2.0,double(bits)) * ( 0 - gsl_log1p( 0 - probability ) ) );
 }
 
 double chooseK ( int b, int k );
