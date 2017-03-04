c0d9c94f710942ae7ac67fa85b3978d53134ad1a
Yves Orton <demerphq@gmail.com>
2017-03-05 00:35:36 +0100

    fixup test scripts

--- mk_test_hashes.sh
+++ mk_test_hashes.sh
@@ -2,17 +2,16 @@
 echo "make"
 echo "rm -f doc/*.tmp"
 echo "export BITS=32"
-./SMHasher --list | 
-    sort -k 2n -k 1 | 
+./SMHasher --list | sort -k 4n -k 1 |
     grep -v '^crc' | 
     grep -v -i 'crap\|nothing\|bad\|sum\|sha1\|md5' |
-    perl -ne'@F=split/[\s|]+/,$_;print qq(
+    perl -ne'@F=split/[\s|]+/,$_; /[A-Z]/ and $F[2]=~/\d/ and print qq(
 #$_
-if [ "x\$BITS" == "x" -o "x\$BITS" == "x$F[1]" ]; then    
-    touch doc/$F[0]_$F[1].out &&
-    ./SMHasher $F[0] 2>&1 | tee doc/$F[0]_$F[1].tmp &&
-    mv doc/$F[0]_$F[1].out doc/$F[0]_$F[1].bak &&
-    mv doc/$F[0]_$F[1].tmp doc/$F[0]_$F[1].out
+if [ "x\$BITS" == "x" -o "x\$BITS" == "x$F[2]" ]; then
+    touch doc/$F[0].$F[2].out &&
+    ./SMHasher $F[0] 2>&1 | tee doc/$F[0].$F[2].tmp &&
+    mv doc/$F[0].$F[2].out doc/$F[0].$F[2].bak &&
+    mv doc/$F[0].$F[2].tmp doc/$F[0].$F[2].out
 fi
 
 )'
