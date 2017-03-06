65538cc9c12a0188a9d74f7b7d6d6d9790646a8a
Yves Orton <demerphq@gmail.com>
2017-03-06 16:23:27 +0100

    lookup3: use the new api for testing

--- lookup3.cpp
+++ lookup3.cpp
@@ -70,3 +70,8 @@ void lookup3_test ( const void * key, int len, uint32_t seed, void * out )
 {
   *(uint32_t*)out = lookup3(key,len,seed);
 }
+
+void lookup3_with_state_test ( const void * key, int len, const void *state, void * out )
+{
+  *(uint32_t*)out = lookup3(key,len,*((uint32_t*)state));
+}
