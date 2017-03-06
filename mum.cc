26003e859f0815bc978c7a8da88e477a579136eb
Yves Orton <demerphq@gmail.com>
2017-03-06 16:21:51 +0100

    mum.cc: support new api

--- mum.cc
+++ mum.cc
@@ -3,3 +3,6 @@
 void mum_hash_test(const void *key, int len, uint32_t seed, void *out) {
   *(uint64_t *)out = mum_hash(key, len, seed);
 }
+void mum_hash_with_state_test(const void *key, int len, const void *state, void *out) {
+  *(uint64_t *)out = mum_hash(key, len, *((uint64_t*)state));
+}
