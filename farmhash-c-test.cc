d62adf3cb5193ab635fbf2c2bb662022a83f561d
Yves Orton <demerphq@gmail.com>
2017-03-06 16:45:44 +0100

    more farmhash updates for the new api (pure c variant)

--- farmhash-c-test.cc
+++ farmhash-c-test.cc
@@ -6,11 +6,24 @@
 void farmhash32_c_test ( const void * key, int len, uint32_t seed, void * out ) {
   *(uint32_t*)out = farmhash32_with_seed((const char *)key,(size_t)len,seed);
 }
+void farmhash32_c_with_state_test ( const void * key, int len, const void *state, void * out ) {
+  *(uint32_t*)out = farmhash64_with_seed((const char *)key,(size_t)len,*((uint64_t*)state));
+}
+
 void farmhash64_c_test ( const void * key, int len, uint32_t seed, void * out ) {
   *(uint64_t*)out = farmhash64_with_seed((const char *)key,(size_t)len,(uint64_t)seed);
 }
+void farmhash64_c_with_state_test ( const void * key, int len, const void *state, void * out ) {
+  *(uint64_t*)out = farmhash64_with_seed((const char *)key,(size_t)len,*((uint64_t*)state));
+}
+
 void farmhash128_c_test ( const void * key, int len, uint32_t seed, void * out ) {
   uint128_c_t s = make_uint128_c_t(seed, 0UL);
   uint128_c_t result = farmhash128_with_seed((const char *)key, (size_t)len, s);
+  memcpy(out, &result, 128/8);
+}
+
+void farmhash128_c_with_state_test ( const void * key, int len, const void *state, void * out ) {
+  uint128_c_t result = farmhash128_with_seed((const char *)key, (size_t)len, *((uint128_c_t *)state));
   memcpy(out, &result, 128/8);
 }
