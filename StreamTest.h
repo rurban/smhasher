6d7379ce36f21d071553dae326c4320e0c831945
Yves Orton <demerphq@gmail.com>
2017-03-06 16:47:24 +0100

    use a different api to hash in CtrStream

--- StreamTest.h
+++ StreamTest.h
@@ -8,7 +8,8 @@ void CtrStream(hashfunc<hashtype>hash, uint64_t rng_seed, int key_len) {
     memset(key,0,key_len);
     hash.seed_state_rand(r);
     while (1) {
-        hashtype h= hash(key,key_len);
+        hashtype h;
+        hash(key,key_len,&h);
         std::cout.write((const char*)&h,sizeof(h));
         int i= 0;
         while (1) {
