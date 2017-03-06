79964767b157bbe09fa7355ee87bc3bdadef1940
Yves Orton <demerphq@gmail.com>
2017-03-06 16:25:45 +0100

    use new api for testing Marvin32

--- Marvin32.h
+++ Marvin32.h
@@ -6,7 +6,7 @@ extern "C" {
 #include <stdio.h>
 #include <stdint.h>
 
-void marvin_32_smhasher_test(const void *key, int len, uint32_t seed_base, void *out);
+void marvin_32_smhasher_test(const void *key, int len, const void *state, void *out);
 
 #ifdef __cplusplus
 }
