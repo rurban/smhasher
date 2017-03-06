5e764175cc3cc23f7ba39246afe5f828404f075b
Yves Orton <demerphq@gmail.com>
2017-03-06 16:48:35 +0100

    fixup the update_hashes.pl script to handle new struct format

--- update_hashes.pl
+++ update_hashes.pl
@@ -5,9 +5,11 @@ my %fix;
 chomp(my @broken= `./SMHasher --validate`);
 foreach my $line (@broken) {
     if ($line=~/^(\w+)\s+- Verification value (0x\S+)/) {
+        print "Must update $1 to $2\n";
         $fix{$1}= $2;
     }
 }
+exit 0 if !%fix;
 
 my $contents= do { local(@ARGV,$/)=("main.cpp"); <>};
 my $hashes= 
@@ -15,8 +17,41 @@ $contents=~/HashInfo\s+g_hashes\[\]\s*=\s*\{(.*?)\};/s;
 my $inner= $1;
 
 sub _format {
+    my $hex= $fix{$_[0]} || $_[5];
     return 
-      "{ $_[7], $_[8],\n" . 
+  qq'{ "$_[0]", "$_[1]",\n' .
+    "    $_[2], $_[3], $_[4], $hex,\n".
+    "    $_[6], $_[7], $_[8] }";
+}
+
+# { mum_hash_test, NULL, NULL, 32, 32, 64, MUM_VERIFY, "MUM",
+#     "github.com/vnmakarov/mum-hash", NULL }
+$inner=~s/\{
+           \s*"([^"]+)"\s*,
+           \s*"([^"]+)"\s*,
+           \s*([^,]+?)\s*,
+           \s*([^,]+?)\s*,
+           \s*([^,]+?)\s*,
+           \s*([^,]+?)\s*,
+           \s*(\w+)\s*,
+           \s*(\w+)\s*,
+           \s*(\w+)\s*
+           \}/
+            _format($1,$2,$3,$4,$5,$6,$7,$8,$9)
+        /xge or die "Failed!";
+
+$contents=~s/(HashInfo\s+g_hashes\[\]\s*=\s*\{)(.*?)(\};)/$1$inner$3/s;
+
+open my $fh,">", "main.cpp"
+    or die "Failed to open main.cpp for write: $!";
+print $fh $contents;
+close $fh;
+exit 0+keys %fix;
+__END__
+
+sub _format {
+    return
+      "{ $_[7], $_[8],\n" .
     "    $_[3], $_[4], $_[5], $_[6],\n".
     "    $_[1], $_[2], $_[0] }";
 }
@@ -29,11 +64,3 @@ $inner=~s/\{\s*(\w+)\s*,\s*(\w+)\s*,\s*(\w+)\s*,
            \s*("[^"]+")\s*(?:,\s*(\w+)\s*)?\}/
             _format($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)
         /xge;
-
-$inner=~s/(0x[A-Fa-f0-9]+)(\s*,\s*"([^"]+)")/($fix{$3}||$1).$2/ge;
-$contents=~s/(HashInfo\s+g_hashes\[\]\s*=\s*\{)(.*?)(\};)/$1$inner$3/s;
-
-open my $fh,">", "main.cpp"
-    or die "Failed to open main.cpp for write: $!";
-print $fh $contents;
-close $fh;
