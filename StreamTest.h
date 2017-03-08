f304c75f52d2d3abe4326e1cdefd4bf7e005213b
Yves Orton <demerphq@gmail.com>
2017-03-08 14:22:03 +0100

    better StreamTest implementation (that works)

--- StreamTest.h
+++ StreamTest.h
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <iostream>
 template <typename hashtype>
-void CtrStream(hashfunc<hashtype>hash, uint64_t rng_seed, int key_len) {
+void CtrStream(hashfunc<hashtype> hash, uint64_t rng_seed, int key_len) {
     uint8_t key[key_len];
+    char out[sizeof(hashtype)];
     Rand r(rng_seed);
     memset(key,0,key_len);
     hash.seed_state_rand(r);
     while (1) {
-        hashtype h;
-        hash(key,key_len,&h);
-        std::cout.write((const char*)&h,sizeof(h));
+        hash(key,key_len,out);
+        std::cout.write(out,sizeof(hashtype));
         int i= 0;
         while (1) {
             if ( ++key[i] ) {
