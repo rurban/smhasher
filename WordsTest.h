a34485135120ee1f0d6bfec2f31d6179540c873a
Yves Orton <demerphq@gmail.com>
2017-03-15 09:15:12 +0100

    increase number of expected collisions in WordsTest
    also clean it up in various ways.

--- WordsTest.h
+++ WordsTest.h
@@ -3,24 +3,36 @@
 #include <fstream>
 template < typename hashtype >
 bool WordsTest( hashfunc<hashtype> hash, double confidence) {
-    std::vector<hashtype> hashes(100000);
+    std::vector<hashtype> hashes(200000);
     std::string line;
     std::string filename= "/usr/share/dict/words";
+    int count= 0;
+    int lines= 0;
+    Rand r(82762);
+    hash.seed_state_rand(r);
     std::ifstream wordfile (filename.c_str());
-    int count;
-    if (wordfile.is_open())
+    if (!wordfile.is_open())
     {
-        while ( getline (wordfile,line) )
-        {
-            if (count >= hashes.size()) hashes.resize(count+100);
-            hash(line.c_str(),line.length(),&hashes[count++]);
-        }
-        wordfile.close();
-        hashes.resize(count);
-        printf("# Read %d lines from %s\n", count, filename.c_str());
-    } else {
-        std::cout << "Unable to open word file"; 
+        std::cout << "Unable to open word file " << filename << "\n";
         exit(1);
     }
+    while ( getline (wordfile,line) )
+    {
+        lines++;
+        if (count+3 >= hashes.size())
+            hashes.resize(count+100);
+
+        hash(line.c_str(),line.length(),&hashes[count++]);
+
+        line.append("!");
+        hash(line.c_str(),line.length(),&hashes[count++]);
+
+        line.append("!");
+        hash(line.c_str(),line.length(),&hashes[count++]);
+    }
+    wordfile.close();
+    hashes.resize(count);
+    printf("# Hashed %d keys from %d words from file '%s'\n",
+            lines, count, filename.c_str());
     return TestHashList<hashtype>(hashes,true,confidence,false,"Keyset 'Words'");
 }
