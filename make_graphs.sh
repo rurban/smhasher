69ed3ed30781b9368b26e09a6a819b110d93ed12
Yves Orton <demerphq@gmail.com>
2017-03-15 13:29:20 +0100

    update graphs with latest data, etc

--- make_graphs.sh
+++ make_graphs.sh
@@ -7,6 +7,7 @@ ls doc/cmetro*.out doc/metro*.out | xargs perl r2p.pl --name "MetroHash Family"
 ls doc/City*.out | xargs perl r2p.pl
 ls doc/Farm*.out doc/farm*.out | xargs perl r2p.pl
 ls doc/t1ha*.out | xargs perl r2p.pl
+ls doc/City*.64.out doc/*metro*.64.out doc/StadtX.*.64.out doc/Zaphod64.*.64.out doc/Sip*.64.out doc/Farm*.64.out doc/t1ha*.64.out doc/xxHash64.*.64.out | xargs perl r2p.pl --name "Selected 64 Bit"
 
 
 
