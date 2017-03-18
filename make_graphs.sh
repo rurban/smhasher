9a2a0ac494ba06d4d76885a784a9f3989a7f5525
Yves Orton <demerphq@gmail.com>
2017-03-18 18:41:40 +0100

    commit exclude sha1 based hashes from Passed_All_Tests

--- make_graphs.sh
+++ make_graphs.sh
@@ -9,7 +9,7 @@ ls doc/City*.txt | xargs perl r2p.pl
 ls doc/Farm*.txt doc/farm*.txt | xargs perl r2p.pl
 ls doc/t1ha*.txt | xargs perl r2p.pl
 ls doc/City*.64.txt doc/*metro*.64.txt doc/StadtX.*.64.txt doc/Zaphod64.*.64.txt doc/Sip*.64.txt doc/Farm*.64.txt doc/t1ha*.64.txt doc/xxHash64.*.64.txt | xargs perl r2p.pl --name "Selected 64 Bit"
-ls $(grep PASSED doc/summary.txt | cut -d' ' -f1 | perl -lne'print "doc/$_.*.txt"')  | xargs perl r2p.pl --name 'Passed All Tests'
+ls $(grep PASSED doc/summary.txt | cut -d' ' -f1 | grep -v sha1 | perl -lne'print "doc/$_.*.txt"')  | xargs perl r2p.pl --name 'Passed All Tests'
 ls doc/sdbm.*.txt doc/*Sip*.txt doc/StadtX.*.txt doc/Zaphod*.txt  | xargs perl r2p.pl --name 'SDBM Selected Comparison'
 ls doc/Jenkins*.txt doc/Sip*.txt doc/StadtX*.txt | xargs perl r2p.pl --name 'Perl Hash Comparison'
 
