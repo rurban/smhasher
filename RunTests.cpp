a15ea007506bad6c3bcf7bf4f52127dee3a313c7
Yves Orton <demerphq@gmail.com>
2017-03-18 13:55:23 +0100

    Avalanche test 520 bit keys

--- RunTests.cpp
+++ RunTests.cpp
@@ -254,6 +254,10 @@ bool testHash ( HashInfo * info, int self_test, double confidence )
     result &= ok(AvalancheTest< Blob<512>, hashtype > (
           hash, reps, r, confidence, max_pct_error, max_error_ratio),
         "Strict Avalanche Criteria - 512 bit keys");
+    if (!size || size == 520)
+    result &= ok(AvalancheTest< Blob<520>, hashtype > (
+          hash, reps, r, confidence, max_pct_error, max_error_ratio),
+        "Strict Avalanche Criteria - 520 bit keys");
 
     pass &= ok(result, "Strict Avalanche Criteria", info->name);
   }
