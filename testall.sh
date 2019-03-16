16776b882ae050827a1be802b9d29cc6b8362878
Reini Urban <rurban@cpan.org>
2019-03-16 18:19:00 +0100

    testall.sh: create the docs

--- testall.sh
+++ testall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make -C build
 test -f log.hashes && mv log.hashes log.hashes.bak
-(for g in `build/SMHasher --list | cut -s -f1`; do build/SMHasher $g; done) | tee log.hashes
+(for g in `build/SMHasher --list | cut -s -f1`; do build/SMHasher $g | tee doc/$g; done) | tee log.hashes
