3f84184a9c6e0c2c8b0de46887f011ee05d2297b
Reini Urban <rurban@cpan.org>
2019-03-18 14:08:36 +0100

    add testextra.sh
    --extra tests for the best small hashes

--- testpar.sh
+++ testpar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make -C build
 build/SMHasher --list | perl -alne 'print $F[0]' | \
-    parallel -j4 --bar 'build/SMHasher {} | tee doc/{}'
+    parallel -j4 --bar 'build/SMHasher {} >doc/{}'
