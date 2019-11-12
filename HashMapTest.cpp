3f6dff4b406f58595e86d937868b4ca3b08d6176
Reini Urban <rurban@cpan.org>
2019-11-12 10:14:45 +0100

    WIP HashMapTest + HashMapInit

--- HashMapTest.cpp
+++ HashMapTest.cpp
@@ -1,47 +1,69 @@
 #include "Platform.h"
 #include "HashMapTest.h"
+#include "Random.h"
+
+#include <string>
+#include <unordered_map>
+#include <functional>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 //-----------------------------------------------------------------------------
 // This should be a realistic I-Cache test, when our hash is used inlined
 // in a hash table. There the size matters more than the bulk speed.
 
-bool HashMapTest ( pfHash hash, const int hashbits, bool verbose )
+std::set<std::string> HashMapInit(bool verbose) {
+  std::set<std::string> words;
+  std::set<std::string>::iterator it;
+  std::string line;
+  std::string filename = "/usr/share/dict/words";
+  int lines = 0, sum = 0;
+  std::ifstream wordfile(filename.c_str());
+  if (!wordfile.is_open()) {
+    std::cout << "Unable to open words dict file " << filename << "\n";
+    return words;
+  }
+  while (getline(wordfile, line)) {
+    int len = line.length();
+    lines++;
+    sum += len;
+    words.insert(line);
+    //line.append("!");
+    //words.insert(line);
+    //line.append("!");
+    //words.insert(line);
+  }
+  wordfile.close();
+  if (verbose) {
+    printf ("Read %d words from '%s'\n", lines, filename.c_str());
+    printf ("Avg len: %0.3f\n", (sum+0.0)/lines);
+  }
+  return words;
+}
+
+bool HashMapTest ( pfHash pfhash, std::set<std::string> words,
+                   const int hashbits, bool verbose )
 {
   const int hashbytes = hashbits / 8;
-
-  uint8_t * key    = new uint8_t[256];
-  uint8_t * hashes = new uint8_t[hashbytes * 256];
-  uint8_t * final  = new uint8_t[hashbytes];
-
-  memset(key,0,256);
-  memset(hashes,0,hashbytes*256);
-  memset(final,0,hashbytes);
-
+  Rand r(82762);
+  const uint32_t seed = r.rand_u32();
+  unordered_map<string,int,function<size_t ( const string &key)>>
+    hashmap(words.size(), []( const string &key)
+                  {
+                    const void* out;
+                    size_t result;
+                    pfhash(key.c_str(), key.length(), seed, out);
+                    memcpy(&result, out, hashbits);
+                    return result;
+                  });
+  
   printf("Running HashMapTest     ");
-#if 0
-  // Hash keys of the form {0}, {0,1}, {0,1,2}... up to N=255,using 256-N as
-  // the seed
-  for(int i = 0; i < 256; i++)
-  {
-    key[i] = (uint8_t)i;
-    hash(key,i,256-i,&hashes[i*hashbytes]);
-  }
-
-  // Then hash the result array
-  hash(hashes,hashbytes*256,0,final);
-
-  // The first four bytes of that hash, interpreted as a little-endian integer, is our
-  // verification value
-  uint32_t verification =
-      (final[0] << 0) | (final[1] << 8) | (final[2] << 16) | (final[3] << 24);
-#endif
-
-  delete [] key;
-  delete [] hashes;
-  delete [] final;
 
   //----------
 
+  delete hashmap;
   printf(" ....... PASS\n");
   return true;
 }
