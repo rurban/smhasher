cd4aea1b81c1cb0742deaa45cf5873b122db438b
Reini Urban <rurban@cpan.org>
2019-12-06 20:24:52 +0100

    use --listnames

--- sanity.sh
+++ sanity.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ./build.sh
-(for g in `build/SMHasher --list | cut -s -f1`; do build/SMHasher --test=Sanity $g; done) \
+(for g in `build/SMHasher --listnames`; do build/SMHasher --test=Sanity $g; done) \
 	| tee log.sanity
 ./build32.sh
-(for g in `build32/SMHasher --list | cut -s -f1`; do build32/SMHasher --test=Sanity $g; done) \
+(for g in `build32/SMHasher --listnames`; do build32/SMHasher --test=Sanity $g; done) \
 	| tee -a log.sanity
--- testhashmaps.sh
+++ testhashmaps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 make -C build
 mv log.hashmap log.hashmap~;
-for g in `build/SMHasher --list | cut -s -f1 | tail -n +15`
+for g in `build/SMHasher --listnames | tail -n +15`
   do build/SMHasher --test=Hashmap $g
 done | tee log.hashmap
 
