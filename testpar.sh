d95cce91d0e5b814bc7fd1872f6c5c8289f0a49d
Reini Urban <rurban@cpan.org>
2020-03-07 16:33:36 +0100

    add 2 new tests to testpar*.sh

--- testpar.sh
+++ testpar.sh
@@ -4,6 +4,6 @@ mkdir partests
 test -n "$@" && r="if /$@/"
 build/SMHasher --list|perl -alne"print \$F[0] $r" | \
     parallel -j4 --bar \
-      'build/SMHasher --test=Sparse,Permutation,Cyclic,TwoBytes,DiffDist,Text,Zeroes,Seed,Sanity,Avalanche,BIC,Diff,MomentChi2 {} 2>&1 >partests/{}'
+      'build/SMHasher --test=Sparse,Permutation,Cyclic,TwoBytes,DiffDist,Text,Zeroes,Seed,Perlin,Sanity,Avalanche,BIC,Diff,MomentChi2,Prng {} 2>&1 >partests/{}'
 
 ./fixupdoctests.pl
--- testpar2.sh
+++ testpar2.sh
@@ -4,6 +4,6 @@ mkdir partests2
 test -n "$@" && r="if /$@/"
 build/SMHasher --list|perl -alne"print \$F[0] $r" | \
     parallel -j4 --bar \
-      'build/SMHasher --test=Seed,Sanity,Avalanche,BIC,Diff,MomentChi2 {} 2>&1 >partests2/{}'
+      'build/SMHasher --test=Sanity,Avalanche,Seed,Perlin,Diff,BIC,MomentChi2,Prng {} 2>&1 >partests2/{}'
 
 ./fixupdoctests.pl partests2/*
