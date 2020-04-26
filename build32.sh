3a29bea6babdb8ed0dcf282d99e416db490bc6eb
Reini Urban <rurban@cpan.org>
2020-04-26 19:24:36 +0200

    build32: use gcc-7 as linux cross-compiler

--- build32.sh
+++ build32.sh
@@ -4,7 +4,7 @@ mkdir build32
 cd build32
 case `uname -s` in
 Darwin) CXX="/usr/bin/c++ -m32" CC="/usr/bin/cc -m32" cmake .. $@ ;;
-*)      CXX="g++ -m32" CC="gcc -m32" cmake .. $@ ;;
+*)      CXX="g++-7 -m32" CC="gcc-7 -m32" cmake .. $@ ;;
 esac
 make
 cd ..
