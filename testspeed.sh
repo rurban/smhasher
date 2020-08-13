01fb4d8ede633936b65aff03a38b55aa2d897211
Reini Urban <rurban@cpan.org>
2020-08-13 09:36:38 +0200

    fixup prvhash speeds from my bench machine
    which is usually much slower and stable. to be comparable to the others.

--- testspeed.sh
+++ testspeed.sh
@@ -3,12 +3,12 @@ make -C build
 which performance && performance
 if [ -z "$@" ]; then
     test -f log.speed && mv log.speed log.speed.bak
-    (for g in `build/SMHasher --listnames`; do
+    (for g in `build/SMHasher --listnames`; do \
          build/SMHasher --test=Speed,Hashmap $g 2>&1; done) | tee log.speed
     ./speed.pl && \
         ./fixupdocspeeds.pl
 else
-     (for g in `build/SMHasher --listnames | egrep "$@"`; do
+     (for g in `build/SMHasher --listnames | egrep "$@"`; do \
          build/SMHasher --test=Speed,Hashmap $g 2>&1; done) | tee "log.speed-$1"
     ./speed.pl "log.speed-$1" && \
         ./fixupdocspeeds.pl "log.speed-$1"
