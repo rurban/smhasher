69c57487b8ffd47bd8a15e8ad92e68d9bb425338
Thomas Dybdahl Ahle <thomas@ahle.dk>
2020-09-03 12:19:00 +0200

    New implementations of multiply shift

--- Hashes.h
+++ Hashes.h
@@ -465,6 +465,8 @@ void clhash_seed_init(size_t seed);
 void clhash_test (const void * key, int len, uint32_t seed, void * out);
 #endif
 
+void multiply_shift_init();
+void multiply_shift_seed_init(size_t seed);
 void multiply_shift (const void * key, int len, uint32_t seed, void * out);
 void pair_multiply_shift (const void *key, int len, uint32_t seed, void *out);
 
