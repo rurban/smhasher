2c4838e663f21bcd8847f54e214a689ec4d0e41b
Reini Urban <rurban@cpan.org>
2020-09-11 11:47:45 +0200

    add CMakeARM64Cross.txt/aarch64-linux-gnu support
    and simplify the timers a bit. the aarch64 asm code was wrong,
    needed to seperate it from arm32. now it should work on phones

--- Platform.h
+++ Platform.h
@@ -56,6 +56,9 @@ void SetAffinity ( int cpu );
 
 #else	//	!defined(_MSC_VER)
 
+#if !defined (__i386__) && !defined (__x86_64__)
+#include <cstddef>
+#endif
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/time.h>
@@ -94,16 +97,47 @@ inline uint64_t rotr64 ( uint64_t x, int8_t r )
 #define	ROTR32(x,y)	rotr32(x,y)
 #define ROTR64(x,y)	rotr64(x,y)
 
+__inline__ uint64_t timeofday()
+{
+  struct timeval tv;
+  gettimeofday(&tv, nullptr);
+  return static_cast<int64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
+}
+
 __inline__ uint64_t rdtsc()
 {
 #ifdef _MSC_VER
     return __rdtsc();
 #elif defined (__i386__) || defined (__x86_64__)
     return __builtin_ia32_rdtsc();
+#elif defined(__ARM_ARCH) && (__ARM_ARCH >= 6) && defined(HAVE_INT32)
+  // V6 is the earliest arch that has a standard cyclecount (some say V7)
+  uint32_t pmccntr;
+  uint32_t pmuseren;
+  uint32_t pmcntenset;
+  // Read the user mode perf monitor counter access permissions.
+  asm volatile("mrc p15, 0, %0, c9, c14, 0" : "=r"(pmuseren));
+  if (pmuseren & 1) {  // Allows reading perfmon counters for user mode code.
+    asm volatile("mrc p15, 0, %0, c9, c12, 1" : "=r"(pmcntenset));
+    if (pmcntenset & 0x80000000ul) {  // Is it counting?
+      asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(pmccntr));
+      // The counter is set up to count every 64th cycle
+      return static_cast<int64_t>(pmccntr) * 64;  // Should optimize to << 6
+    }
+  }
+  return timeofday();
+#elif defined(__aarch64__) && defined(HAVE_INT64)
+  uint64_t pmccntr;
+  uint64_t pmuseren = 1UL;
+  // Read the user mode perf monitor counter access permissions.
+  //asm volatile("mrs cntv_ctl_el0,  %0" : "=r" (pmuseren));
+  if (pmuseren & 1) {  // Allows reading perfmon counters for user mode code.
+    asm volatile("mrs %0, cntvct_el0" : "=r" (pmccntr));
+    return (uint64_t)(pmccntr) * 64;  // Should optimize to << 6
+  }
+  return timeofday();
 #else
-  struct timeval tv;
-  gettimeofday(&tv, nullptr);
-  return static_cast<int64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
+  return timeofday();
 #endif
 }
 
@@ -128,22 +162,6 @@ __inline__ uint64_t timer_start()
        "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
        "%rax", "%rbx", "%rcx", "%rdx");
   return ((uint64_t)cycles_high << 32) | cycles_low;
-#elif defined(__ARM_ARCH) && (__ARM_ARCH >= 6)
-  // V6 is the earliest arch that has a standard cyclecount
-  uint32_t pmccntr;
-  uint32_t pmuseren;
-  uint32_t pmcntenset;
-  // Read the user mode perf monitor counter access permissions.
-  asm volatile("mrc p15, 0, %0, c9, c14, 0" : "=r"(pmuseren));
-  if (pmuseren & 1) {  // Allows reading perfmon counters for user mode code.
-    asm volatile("mrc p15, 0, %0, c9, c12, 1" : "=r"(pmcntenset));
-    if (pmcntenset & 0x80000000ul) {  // Is it counting?
-      asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(pmccntr));
-      // The counter is set up to count every 64th cycle
-      return static_cast<int64_t>(pmccntr) * 64;  // Should optimize to << 6
-    }
-  }
-  return rdtsc();
 #else
   return rdtsc();
 #endif
