1baeeb262b62a3e222cd15cf5f46c412e9c74289
Reini Urban <rurban@cpan.org>
2020-09-24 16:23:58 +0200

    add 32bit benchmarks (i686)

--- build32.sh
+++ build32.sh
@@ -4,7 +4,7 @@ mkdir build32
 cd build32
 case `uname -s` in
 Darwin) CXX="/usr/bin/c++ -m32" CC="/usr/bin/cc -m32" cmake .. $@ ;;
-*)      CXX="g++-7 -m32" CC="gcc-7 -m32" cmake .. $@ ;;
+*)      CXX="i686-linux-gnu-g++" CC="i686-linux-gnu-gcc" cmake .. $@ ;;
 esac
 make
 cd ..
---
+++ testspeed32.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+make -C build32
+which performance && performance
+if [ -z "$@" ]; then
+    test -f log.speed32 && mv log.speed32 log.speed32.bak
+    (for g in `build32/SMHasher --listnames`; do \
+         build32/SMHasher --test=Speed,Hashmap $g 2>&1; done) | tee log.speed32
+    ./speed.pl -h=doc/i686 log.speed32
+else
+    for g in `build32/SMHasher --listnames`; do
+        for p in $@; do
+             if [[ $g =~ *$p* ]]; then
+                 build32/SMHasher --test=Speed,Hashmap $g 2>&1
+             fi
+        done
+     done | tee "log.speed32-$1"
+    ./speed.pl -h=doc/i686 "log.speed32-$1"
+fi
