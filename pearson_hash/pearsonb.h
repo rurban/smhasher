84b5d7ee5f2cf2b639ff0e8755109239d3d4e1b7
Logan oos Even <Logan.00sEven@gmail.com>
2021-05-08 00:16:11 +0545

    made Pearson B seedable

--- pearson_hash/pearsonb.h
+++ pearson_hash/pearsonb.h
@@ -4,14 +4,14 @@
 #include "portable_endian.h"
 
 
-void pearsonb_hash_256 (uint8_t *out, const uint8_t *in, size_t len);
+void pearsonb_hash_256 (uint8_t *out, const uint8_t *in, size_t len, uint64_t seed);
 
-void pearsonb_hash_128 (uint8_t *out, const uint8_t *in, size_t len);
+void pearsonb_hash_128 (uint8_t *out, const uint8_t *in, size_t len, uint64_t seed);
 
-uint64_t pearsonb_hash_64 (const uint8_t *in, size_t len);
+uint64_t pearsonb_hash_64 (const uint8_t *in, size_t len, uint64_t seed);
 
-uint32_t pearsonb_hash_32 (const uint8_t *in, size_t len);
+uint32_t pearsonb_hash_32 (const uint8_t *in, size_t len, uint64_t seed);
 
-uint16_t pearsonb_hash_16 (const uint8_t *in, size_t len);
+uint16_t pearsonb_hash_16 (const uint8_t *in, size_t len, uint64_t seed);
 
 void pearsonb_hash_init(void);
