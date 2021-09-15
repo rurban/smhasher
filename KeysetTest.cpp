365f6f5b6c7192d44e8d8c8ec63834217d99373f
Frank J. T. Wojcik <fwojcik@uw.edu>
2021-09-14 22:38:23 -0700

    CLEANUP: Revert to lowercase printout for expected hash during verification failures
    This is for easier human identification of which value is expected
    versus computed.

--- KeysetTest.cpp
+++ KeysetTest.cpp
@@ -60,7 +60,7 @@ bool VerificationTest ( HashInfo* info, bool verbose )
       return true;
     } else {
       if (verbose)
-        printf("Verification value 0x%08X ....... FAIL! (Expected 0x%08X)\n",
+        printf("Verification value 0x%08X ....... FAIL! (Expected 0x%08x)\n",
                verification, expected);
       return false;
     }
