864a14165e0eee9c0d63809c89ebb58bacf923ab
Reini Urban <rurban@cpan.org>
2022-08-12 10:40:57 +0200

    run ./sanity.sh

--- sanity.sh
+++ sanity.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 ./build.sh
 (for g in `build/SMHasher --listnames`; do build/SMHasher --test=Sanity $g; done) \
-	| tee log.sanity
+	2>&1 | tee log.sanity
 ./build32.sh
 (for g in `build32/SMHasher --listnames`; do build32/SMHasher --test=Sanity $g; done) \
-	| tee -a log.sanity
+	2>&1 | tee -a log.sanity
 make -C build-asan
 (for g in `build-asan/SMHasher --listnames`; do build-asan/SMHasher --test=Sanity $g; done) \
-	| tee -a log.sanity
+	2>&1 | tee -a log.sanity
