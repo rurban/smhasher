4f2217644e43fb1729fdb7ec0e7990d6278a1934
tansy <wrotycz@wir.pl>
2023-10-04 01:34:47 +0000

    wyhash fv4.1 https://github.com/wangyi-fudan/wyhash/commit/e92bc3e9adf862aad364ff74a24ef2cae854b0b1

--- wyhash.h
+++ wyhash.h
@@ -8,8 +8,8 @@
    uint64_t hash=wyhash(s.c_str(), s.size(), 0, _wyp);
 */
 
-#ifndef wyhash_final_version_4
-#define wyhash_final_version_4
+#ifndef wyhash_final_version_4.1
+#define wyhash_final_version_4.1
 // Version 4.1
 
 #ifndef WYHASH_CONDOM
@@ -190,15 +190,15 @@ static inline uint64_t wyhash(const void *key, size_t len, uint64_t seed, const 
     else a=b=0;
   }
   else{
-    size_t i=len; 
-    if(_unlikely_(i>48)){
+    size_t i=len;
+    if(_unlikely_(i>=48)){
       uint64_t see1=seed, see2=seed;
       do{
         seed=_wymix(_wyr8(p)^secret[1],_wyr8(p+8)^seed);
         see1=_wymix(_wyr8(p+16)^secret[2],_wyr8(p+24)^see1);
         see2=_wymix(_wyr8(p+32)^secret[3],_wyr8(p+40)^see2);
         p+=48; i-=48;
-      }while(_likely_(i>48));
+      }while(_likely_(i>=48));
       seed^=see1^see2;
     }
     while(_unlikely_(i>16)){  seed=_wymix(_wyr8(p)^secret[1],_wyr8(p+8)^seed);  i-=16; p+=16;  }
