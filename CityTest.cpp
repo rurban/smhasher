5efdfb72dae3a9b233436c4ce9b903b455f1bc60
Timur Olur <timurolur@gmail.com>
2024-06-20 18:06:06 +0300

    Adds seed parameter to CityHash32. - city32's known bad seed is added back to g_hashes.

--- CityTest.cpp
+++ CityTest.cpp
@@ -3,8 +3,7 @@
 
 void CityHash32_test(const void *key, int len, uint32_t seed, void *out) {
   // objsize 0-527: 1319
-  *(uint32_t *)out = CityHash32((const char *)key, len);
-  (void)seed;
+  *(uint32_t *)out = CityHash32WithSeed((const char *)key, len, seed);
 }
 
 void CityHash64_test(const void *key, int len, uint32_t seed, void *out) {
